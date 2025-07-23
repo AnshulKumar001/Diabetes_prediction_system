4bba481bb9effefba9c125901946edeceb51cb20
anshulkumar-web <anshulkumar9368289674@gmail.com>
2025-07-23 11:29:18 +0530

    fix path declaration for the moel

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import streamlit as st
 
 
 # loading the saved model
-loaded_model = pickle.load(open('/home/anshul/Desktop/ML_Projects/Diabetes prediction project /trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 
 
 # creating a function for Prediction
